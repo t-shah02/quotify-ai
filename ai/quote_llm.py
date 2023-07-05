872277f9885d19ef740f916d8a58967bdd9b1997
Tanish Shah <connecttanish@gmail.com>
2023-07-04 23:05:01 -0700

    created an ai package and finished all AI-model related work with relation to the quotes

--- ai/quote_llm.py
+++ ai/quote_llm.py
@@ -2,7 +2,7 @@ import numpy as np
 from langchain.llms import OpenAI
 from langchain import PromptTemplate, LLMChain
 from langchain.schema import Document
-from quote_selector import QuoteSelector
+from ai.quote_selector import QuoteSelector
 from typing import List, Dict
 
 
@@ -15,6 +15,7 @@ class PromptInputsEmpty(Exception):
     def __init__(self, message: str):
         super().__init__(message)
 
+
 class QuoteLLM:
 
     MIN_QUOTE_CATEGORIES = 1
@@ -23,11 +24,13 @@ class QuoteLLM:
     def __init__(self, api_key: str, model_name: str, model_temperature: float,
                  quote_selector: QuoteSelector, quote_categories: np.ndarray[np.str_]):
         self.llm = OpenAI(openai_api_key=api_key,
+                          model=model_name,
                           temperature=model_temperature)
         self.quote_selector = quote_selector
         self.quote_categories = quote_categories
         self.prompt_inputs: None | Dict[str, str] = None
         self.prompt_template: None | PromptTemplate = None
+        self.quote_result: None | str = None
 
     def generate_prompt_template(self) -> None:
         num_quote_categories = np.random.randint(
@@ -50,14 +53,10 @@ class QuoteLLM:
         category_vars_formatted = [
             f'{{{category_var}}}' for category_var in category_vars]
         category_str = ', '.join(
-            category_vars_formatted[0:-1]) + f'and {category_vars_formatted[-1]}' if len(category_vars_formatted) > 1 else \
+            category_vars_formatted[0:-1]) + f' and {category_vars_formatted[-1]}' if len(category_vars_formatted) > 1 else \
             category_vars_formatted[0]
 
-        template = f'''
-        Write a creative, expressive quote with the following themes in mind: {category_str}
-        Feel free to use sample quotes relating to those categories as inspiration in the new quote:
-        {final_quotes_str}
-        '''
+        template = f'Write a creative, expressive quote with the following themes in mind: {category_str} \n Feel free to use sample quotes relating to those categories as inspiration in the new quote: \n {final_quotes_str}'
 
         self.prompt_template = PromptTemplate(
             input_variables=category_vars,
@@ -66,7 +65,7 @@ class QuoteLLM:
         self.prompt_inputs = {category_var: selected_quote_category for category_var,
                               selected_quote_category in zip(category_vars, quote_corpus.keys())}
 
-    def run_chain(self) -> str:
+    def run_chain(self) -> None:
         if self.prompt_template is None:
             raise PromptTemplateEmpty(
                 'The prompt template for this quote llm is empty, please generate it first!')
@@ -78,5 +77,4 @@ class QuoteLLM:
         llm_chain = LLMChain(llm=self.llm, prompt=self.prompt_template)
         result = llm_chain.run(self.prompt_inputs)
 
-        return result
-    
+        self.quote_result = result
--- config.py
+++ config.py
@@ -3,8 +3,9 @@ import os
 import logging
 import pandas as pd
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
-from quote_llm import QuoteLLM
-from quote_selector import Quote, QuoteSelector
+from ai.quote_image import QuoteImageGenerator
+from ai.quote_llm import QuoteLLM
+from ai.quote_selector import Quote, QuoteSelector
 from typing import List
 
 logging.basicConfig(level=logging.INFO)
@@ -18,6 +19,11 @@ RAW_DATA_FILENAME = os.environ['RAW_DATA_FILENAME']
 VECTORDB_COLLECTION_NAME = os.environ['VECTORDB_COLLECTION_NAME']
 VECTORDB_PERSIST_FOLDERNAME = os.environ['VECTORDB_PERSIST_FOLDERNAME']
 NUMBER_OF_SIMILARITY_RESULTS = int(os.environ['NUMBER_OF_SIMILARITY_RESULTS'])
+AI_IMAGES_SAVE_DIRECTORY = os.environ['AI_IMAGES_SAVE_DIRECTORY']
+IMAGE_WIDTH = int(os.environ['IMAGE_WIDTH'])
+IMAGE_HEIGHT = int(os.environ['IMAGE_HEIGHT'])
+STABILITY_API_KEY = os.environ['STABILITY_API_KEY']
+IMAGE_ENGINE = os.environ['IMAGE_ENGINE']
 
 quote_csv_path = os.path.join(RAW_DATA_FOLDER_NAME, RAW_DATA_FILENAME)
 logging.info(msg=f'Loading quotes csv from {quote_csv_path}')
@@ -59,3 +65,11 @@ quote_llm = QuoteLLM(api_key=OPENAI_API_KEY,
                      model_temperature=OPENAI_MODEL_TEMPERATURE,
                      quote_selector=quote_selector,
                      quote_categories=quote_categories)
+
+quote_image_generator = QuoteImageGenerator(key=STABILITY_API_KEY,
+                                            verbose=True,
+                                            engine=IMAGE_ENGINE,
+                                            width=IMAGE_WIDTH,
+                                            height=IMAGE_HEIGHT,
+                                            image_save_folder=AI_IMAGES_SAVE_DIRECTORY)
+
